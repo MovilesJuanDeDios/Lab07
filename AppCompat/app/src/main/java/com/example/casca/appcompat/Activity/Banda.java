364dc8161cb663e161e0d058036d83050b5364fc
GiancarloNavarro <30733744+GiancarloNavarro@users.noreply.github.com>
2018-04-26 17:46:44 -0600

    Commit - bibliografias y boton toolbar

--- AppCompat/app/src/main/java/com/example/casca/appcompat/Activity/Banda.java
+++ AppCompat/app/src/main/java/com/example/casca/appcompat/Activity/Banda.java
@@ -1,6 +1,7 @@
 package com.example.casca.appcompat.Activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,6 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import com.example.casca.appcompat.R;
@@ -34,6 +38,45 @@ public class Banda extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        ImageView imgJohn = findViewById(R.id.imgJohn);
+        imgJohn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/John_Lennon"));
+                startActivity(browserIntent);
+                return true;
+            }
+        });
+
+        ImageView imgPaul = findViewById(R.id.imgPaul);
+        imgPaul.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/Paul_McCartney"));
+                startActivity(browserIntent);
+                return true;
+            }
+        });
+
+        ImageView imgGeorge = findViewById(R.id.imgGeorge);
+        imgGeorge.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/George_Harrison"));
+                startActivity(browserIntent);
+                return true;
+            }
+        });
+
+        ImageView imgRingo = findViewById(R.id.imgRingo);
+        imgRingo.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/Ringo_Starr"));
+                startActivity(browserIntent);
+                return true;
+            }
+        });
     }
 
     @Override
@@ -55,15 +98,12 @@ public class Banda extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCc4K7bAqpdBP8jh1j9XZAww"));
+        startActivity(browserIntent);
 
-
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
